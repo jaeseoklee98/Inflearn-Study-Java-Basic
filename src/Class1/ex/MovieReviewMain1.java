a47085badf038bc1e189701c357963e2bbcefee1
jslee <wotjr4017@gmail.com>
2025-06-20 22:41:09 +0900

    문제와 풀이

---
+++ src/Class1/ex/MovieReviewMain1.java
@@ -0,0 +1,18 @@
+package Class1.ex;
+
+public class MovieReviewMain1 {
+
+    public static void main(String[] args) {
+        MovieReview movieReview1 = new MovieReview();
+        movieReview1.title = "기생충";
+        movieReview1.review = "재밌어요";
+
+        MovieReview movieReview2 = new MovieReview();
+        movieReview2.title = "범죄도시";
+        movieReview2.review = "좋아요";
+
+        System.out.println("영화 제목: " + movieReview1.title + ", 리뷰:" + movieReview1.review);
+        System.out.println("영화 제목: " + movieReview2.title + ", 리뷰:" + movieReview2.review);
+
+    }
+}
